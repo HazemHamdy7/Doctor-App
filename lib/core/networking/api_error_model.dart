638668812422be26dac03809d84d8b6ccedae3d6
Hazem Hamdy <hazemhamdy1215@gmail.com>
2025-04-09 22:14:07 +0200

    Refactor API error handling and result models for improved clarity and functionality

--- lib/core/networking/api_error_model.dart
+++ lib/core/networking/api_error_model.dart
@@ -1,6 +1,10 @@
 import 'package:json_annotation/json_annotation.dart';
 part 'api_error_model.g.dart';
 
+// TODO This class is used to represent the error response from the API.
+
+//? This class is use to Error response from the API.
+
 @JsonSerializable()
 class ApiErrorModel {
   final String? message;
@@ -10,6 +14,5 @@ class ApiErrorModel {
 
   factory ApiErrorModel.fromJson(Map<String, dynamic> json) =>
       _$ApiErrorModelFromJson(json);
-
   Map<String, dynamic> toJson() => _$ApiErrorModelToJson(this);
 }
--- lib/core/networking/api_result.freezed.dart
+++ lib/core/networking/api_result.freezed.dart
@@ -112,10 +112,10 @@ as T,
 
 
 class Failure<T> implements ApiResult<T> {
-  const Failure(this.message);
+  const Failure(this.errorHandler);
   
 
- final  String message;
+ final  ErrorHandler errorHandler;
 
 /// Create a copy of ApiResult
 /// with the given fields replaced by the non-null parameter values.
@@ -127,16 +127,16 @@ $FailureCopyWith<T, Failure<T>> get copyWith => _$FailureCopyWithImpl<T, Failure
 
 @override
 bool operator ==(Object other) {
-  return identical(this, other) || (other.runtimeType == runtimeType&&other is Failure<T>&&(identical(other.message, message) || other.message == message));
+  return identical(this, other) || (other.runtimeType == runtimeType&&other is Failure<T>&&(identical(other.errorHandler, errorHandler) || other.errorHandler == errorHandler));
 }
 
 
 @override
-int get hashCode => Object.hash(runtimeType,message);
+int get hashCode => Object.hash(runtimeType,errorHandler);
 
 @override
 String toString() {
-  return 'ApiResult<$T>.failure(message: $message)';
+  return 'ApiResult<$T>.failure(errorHandler: $errorHandler)';
 }
 
 
@@ -147,7 +147,7 @@ abstract mixin class $FailureCopyWith<T,$Res> implements $ApiResultCopyWith<T, $
   factory $FailureCopyWith(Failure<T> value, $Res Function(Failure<T>) _then) = _$FailureCopyWithImpl;
 @useResult
 $Res call({
- String message
+ ErrorHandler errorHandler
 });
 
 
@@ -164,10 +164,10 @@ class _$FailureCopyWithImpl<T,$Res>
 
 /// Create a copy of ApiResult
 /// with the given fields replaced by the non-null parameter values.
-@pragma('vm:prefer-inline') $Res call({Object? message = null,}) {
+@pragma('vm:prefer-inline') $Res call({Object? errorHandler = null,}) {
   return _then(Failure<T>(
-null == message ? _self.message : message // ignore: cast_nullable_to_non_nullable
-as String,
+null == errorHandler ? _self.errorHandler : errorHandler // ignore: cast_nullable_to_non_nullable
+as ErrorHandler,
   ));
 }
 
