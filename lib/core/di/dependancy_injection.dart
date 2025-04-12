f8bf5ab48431de835d56157f16dc7794d487a10f
Hazem Hamdy <hazemhamdy1215@gmail.com>
2025-04-12 05:02:04 +0200

    Refactor dependency injection and app routing to include LoginCubit for improved state management in login flow

--- lib/core/di/dependancy_injection.dart
+++ lib/core/di/dependancy_injection.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:doctor_app/core/networking/api_service.dart';
 import 'package:doctor_app/core/networking/dio_factory.dart';
 import 'package:doctor_app/features/login/data/repo/login_repo.dart';
+import 'package:doctor_app/features/login/logic/cubit/login_cubit.dart';
 import 'package:get_it/get_it.dart';
 
 final getIt = GetIt.instance;
@@ -13,5 +14,5 @@ Future<void> setupGetIt() async {
 
   // Login
   getIt.registerLazySingleton<LoginRepo>(() => LoginRepo(getIt()));
-  getIt.registerFactory<LoginRepo>(() => LoginRepo(getIt()));
+  getIt.registerLazySingleton<LoginCubit>(() => LoginCubit(getIt()));
 }
--- lib/core/routing/app_router.dart
+++ lib/core/routing/app_router.dart
@@ -1,7 +1,9 @@
 import 'package:doctor_app/core/routing/router.dart';
+import 'package:doctor_app/features/login/logic/cubit/login_cubit.dart';
 import 'package:doctor_app/features/onboarding/on_boarding_screen.dart';
 import 'package:doctor_app/features/login/ui/screen/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppRouter {
   Route generateRoute(RouteSettings settings) {
@@ -10,7 +12,13 @@ class AppRouter {
       case Routes.onBoardingScreen:
         return MaterialPageRoute(builder: (_) => const OnBoardingScreen());
       case Routes.login:
-        return MaterialPageRoute(builder: (_) => LoginScreen());
+        return MaterialPageRoute(
+          builder:
+              (_) => BlocProvider(
+                create: (context) => LoginCubit(),
+                child: LoginScreen(),
+              ),
+        );
       default:
         return MaterialPageRoute(
           builder:
