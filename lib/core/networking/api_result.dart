f8c65f5b69c0912546fbc744f22cb7ae49f95ba9
Hazem Hamdy <hazemhamdy1215@gmail.com>
2025-04-12 04:43:39 +0200

    Add login functionality with API integration and state management
    - Implement LoginRepo for handling login requests
    - Create LoginCubit for managing login state
    - Define LoginState with initial, loading, success, and error states
    - Update login_request_body.g.dart for JSON serialization
    - Adjust dependencies in pubspec.yaml and pubspec.lock for compatibility

--- lib/core/networking/api_result.dart
+++ lib/core/networking/api_result.dart
@@ -1,11 +1,11 @@
-import 'package:doctor_app/core/networking/api_error_handler.dart' show ErrorHandler;
+import 'package:doctor_app/core/networking/api_error_handler.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
-part 'api_result.freezed.dart';
+part    'api_result.freezed.dart';
 
 //! يمكن استخدام هذا الكلاس بدلا من استخدام   مكتبهZ)
 //? The class is used with out library DartZ to make two states of the result  Sueccess or Failure
-@Freezed()
-abstract class ApiResult<T> with _$ApiResult<T> {
+@freezed
+class ApiResult<T> with _$ApiResult<T> {
   const factory ApiResult.success(T data) = Success<T>;
   const factory ApiResult.failure(ErrorHandler errorHandler) = Failure<T>;
 }
---
+++ lib/features/login/logic/cubit/login_cubit.dart
@@ -0,0 +1,25 @@
+import 'package:doctor_app/features/login/data/model/login_request_body.dart';
+import 'package:doctor_app/features/login/data/repo/login_repo.dart';
+import 'package:doctor_app/features/login/logic/cubit/login_state.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class LoginCubit extends Cubit<LoginState> {
+  final LoginRepo _loginRepo;
+  LoginCubit(this._loginRepo) : super(LoginState.initial());
+
+  void emitLoginState(LoginRequestBody loginRequestBody) async {
+    emit(LoginState.loading());
+    final response = await _loginRepo.login(loginRequestBody);
+
+    response.when(
+      success: (loginResponse) {
+        emit(LoginState.success(loginResponse));
+      },
+      failure: (errorHandler) {
+        emit(
+          LoginState.error(message: errorHandler.apiErrorModel.message ?? ''),
+        );
+      },
+    );
+  }
+}
